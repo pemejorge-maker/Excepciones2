e58054bdaa760b0e7512ac4abe9126d23adb61da
pemejorge-maker <pemejorge@gmail.com>
2026-05-12 16:48:46 +0000

    Uso de PrintWriter

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -2,30 +2,33 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class Ejercicio1 {
 
     public static void main(String[] args) {
 
         File archivo = new File("entrada.txt");
-
+        PrintWriter salida = null;
         int a , b;
         Scanner teclado= null;
 
         try 
         {
             teclado = new Scanner(archivo);
-            System.out.println("Escribe el 1er numero");  
+            salida = new PrintWriter("salida.txt");
+            //System.out.println("Escribe el 1er numero");  
             a = teclado.nextInt(); 
-            System.out.println("Escribe el 2do numero");  
+            //System.out.println("Escribe el 2do numero");  
             b = teclado.nextInt(); 
-            System.out.println("La divicion es: "+ a/b);
+            salida.println("La division es: " + a/b);
+            //System.out.println("La divicion es: "+ a/b);
         } 
 
         catch (ArithmeticException e) 
         {
             System.out.println("Division por 0");
-        } 
+        }
 
         catch (InputMismatchException e) 
         {
@@ -34,13 +37,16 @@ public class Ejercicio1 {
         }
 
         catch (FileNotFoundException e){
-            System.out.println("archivo no encontrado");
+            System.out.println("Archivo no encontrado");
         }
 
         finally
         {
             if(teclado != null){
                 teclado.close();
+            }
+            if (salida != null) {
+                salida.close();
             }
         }
         
