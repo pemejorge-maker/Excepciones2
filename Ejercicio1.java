300944a809867f69b50b3d8356a3722434098b9c
pemejorge-maker <pemejorge@gmail.com>
2026-05-12 16:40:12 +0000

    Commit 2

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,28 +1,48 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
-public class Ejercicio1{
+public class Ejercicio1 {
 
-    public static void main(String[] args){
-        int a, b;
-        Scanner teclado = new Scanner (System.in);
+    public static void main(String[] args) {
 
-        try{
-            System.out.println("Escribe el primer numero");
-            a = teclado.nextInt();
-            System.out.println("Escribe el segundo numero");
-            b = teclado.nextInt();
-            System.out.println("La division es: " + a/b);
-        }
-        catch (ArithmeticException e){
+        File archivo = new File("entrada.txt");
+
+        int a , b;
+        Scanner teclado= null;
+
+        try 
+        {
+            teclado = new Scanner(archivo);
+            System.out.println("Escribe el 1er numero");  
+            a = teclado.nextInt(); 
+            System.out.println("Escribe el 2do numero");  
+            b = teclado.nextInt(); 
+            System.out.println("La divicion es: "+ a/b);
+        } 
+
+        catch (ArithmeticException e) 
+        {
             System.out.println("Division por 0");
-        }
-        catch (InputMismatchException e){
+        } 
+
+        catch (InputMismatchException e) 
+        {
             System.out.println("Entrada no valida");
-        }
-        finally{
-            teclado.close();
+
         }
 
+        catch (FileNotFoundException e){
+            System.out.println("archivo no encontrado");
+        }
+
+        finally
+        {
+            if(teclado != null){
+                teclado.close();
+            }
+        }
+        
     }
 }
